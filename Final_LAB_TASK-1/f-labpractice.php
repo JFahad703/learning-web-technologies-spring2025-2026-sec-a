3a77958807b2230d68418e7d4c30e8530cb0b70b
JFAHAD703 <jfahad703@gmail.com>
2026-04-08 17:02:14 +0600

    1st commit

---
+++ Final_LAB_TASK-1/f-labpractice.php
@@ -0,0 +1,75 @@
+<html>
+<body>
+    <h1> Test </h1>
+</body>
+</html>
+
+<?php
+     /* $name = "fahad";
+     $id = 17;
+     $cgpa = 3.68;
+
+     $std = [17, "fahad", 3.68];
+     $std = array(17, "fahad", 3.68);
+     $std[17];
+
+     $std = [
+        [17, "fahad", 3.68],
+        [17, "fahad", 3.68],
+        [17, "fahad", 3.68]
+     ];
+
+     $std[17] [2];
+ 
+     $std = ['id' => 17, 'name' => "fahad", 'cgpa' => 3.68];
+     $std = ['cpga'];
+
+
+     $stds = [
+        ['id' => 17, 'name' => "fahad", 'cgpa' => 3.68],
+        ['id' => 18, 'name' => "fahid", 'cgpa' => 3.58],
+        ['id' => 19, 'name' => "fahim", 'cgpa' => 3.48]
+     ];
+
+     $stds[18] ['cgpa'];
+
+      $stds = [
+        's1'['id' => 17, 'name' => "fahad", 'cgpa' => 3.68],
+        's2'['id' => 18, 'name' => "fahid", 'cgpa' => 3.58],
+        's3'['id' => 19, 'name' => "fahim", 'cgpa' => 3.48]
+     ];
+
+     $stds['s2'] ['cgpa'];
+
+     $std = [1, ' fahad', 3.68];
+     for ($i = 0; $i < count($std); $i++) {
+           $std[$i];
+     }
+     foreach ($std as $value) {
+        $value;  
+     }
+
+     if($std[2] > 3.5) {
+        "good";
+     } else {
+        "bad";
+     } */
+
+
+     /*   echo "Hello PHP";
+
+        echo "Hello PHP"." another test....".$name;
+        echo "Hello PHP{$name} another test....";
+       */
+
+        /*print("Hello".$name);
+        print("Hello {$name}");
+
+        $std = ['id' => 17, 'name' => "fahad", 'cgpa' => 3.68];
+        print_r($std);
+        var_dump($std);
+*/
+
+?>
+
+<h2>Test</h2>
---
+++ Final_LAB_TASK-1/f-labtask-2.php
@@ -0,0 +1,6 @@
+<?php
+$amnt = 1000;
+$vat = $amnt * 0.15;
+echo "Original Amount: $" . $amnt . "<br>";
+echo "VAT (15%): $" . $vat;
+?>
