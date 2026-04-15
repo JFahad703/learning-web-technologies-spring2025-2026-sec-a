52ae2520625bf713c10ee656d45a7200616cebed
JFAHAD703 <jfahad703@gmail.com>
2026-04-15 16:04:12 +0600

    classwork

---
+++ Final_LAB_TASK-2/upload.php
@@ -0,0 +1,81 @@
+<?php
+
+
+       //echo "Test!";
+
+       //get method
+/*       print_r($_GET);
+       $username = $_GET["username"];
+       $password = $_GET["password"];
+
+       if ($username == "" || $password == "") {
+
+                echo "Null username or password";
+        } else {
+
+            if($username == $password) {
+                echo "valid user " ;
+            } else {
+                echo "invalid user";
+            }
+        }
+*/
+//post method
+ /*      $username = $_POST["username"];
+       $password = $_POST["password"];
+
+       if ($username == "" || $password == "") {
+
+                echo "Null username or password";
+        } else {
+
+            if($username == $password) {
+                echo "valid user " ;
+            } else {
+                echo "invalid user";
+            }
+        }
+*/
+
+//REQUEST METHOD: IF THE DEVELOPER IS NOT SURE ABOUT THE METHOD, HE CAN USE REQUEST METHOD. IT CAN HANDLE BOTH GET AND POST METHOD.
+      
+
+/*if(isset($_REQUEST['submit'])){
+$username = $_REQUEST["username"];
+$password = $_REQUEST["password"];
+
+       if ($username == "" || $password == "") {
+
+                echo "Null username or password";
+        } else {
+
+            if($username == $password) {
+                echo "valid user " ;
+            } else {
+                echo "invalid user";
+            }
+        }
+} else {
+    echo "Please submit the form";
+}*/
+
+//TO stop the bypass from the URL, WE CAN USE REQUEST METHOD. IF THE USER TRY TO BYPASS THE FORM,it will not get bypassed as it is in post method so get request will not work .
+if(isset($_POST['submit'])){
+$username = $_REQUEST["username"];
+$password = $_REQUEST["password"];
+
+       if ($username == "" || $password == "") {
+
+                echo "Null username or password";
+        } else {
+
+            if($username == $password) {
+                echo "valid user " ;
+            } else {
+                echo "invalid user";
+            }
+        }
+} else {
+    echo "Please submit the form";
+}
+?>       
